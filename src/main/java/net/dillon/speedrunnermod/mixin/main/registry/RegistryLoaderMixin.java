fd553568f56d6ba277666b4ac7aedba8614f07a8
Dillon <dilloneystrickland12@gmail.com>
2025-07-22 12:40:56 -0400

    fix bug with raid eradicator

--- src/main/java/net/dillon/speedrunnermod/mixin/main/registry/RegistryLoaderMixin.java
+++ src/main/java/net/dillon/speedrunnermod/mixin/main/registry/RegistryLoaderMixin.java
@@ -14,7 +14,6 @@ import net.minecraft.registry.RegistryOps;
 import net.minecraft.registry.entry.RegistryEntryInfo;
 import net.minecraft.resource.Resource;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
