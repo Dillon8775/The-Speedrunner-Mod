39a1b29d43bbf230c803eba51b35ba5106ec45f7
Dillon <dilloneystrickland12@gmail.com>
2026-01-10 14:22:42 -0500

    need to fix bug

--- src/main/java/net/dillon/speedrunnermod/main/SpeedrunnerMod.java
+++ src/main/java/net/dillon/speedrunnermod/main/SpeedrunnerMod.java
@@ -38,7 +38,7 @@ import static net.dillon.speedrunnermod.option.ModOptions.isDoomMode;
  * The home initializer for the Speedrunner Mod.
  */
 public class SpeedrunnerMod implements ModInitializer {
-    public static final String MOD_VERSION = "v1.11.9";
+    public static final String MOD_VERSION = "v1.12";
     public static final String MC_VERSION = "1.21.11";
     public static final String VERSION = "Version: " + MOD_VERSION;
     public static final String THE_SPEEDRUNNER_MOD_STRING = "The Speedrunner Mod";
